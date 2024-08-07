217b8f3b533a84f6e09f10a3fe527d743a303db3
Mohamed-Walid Ben Yahia <96090291+leweth@users.noreply.github.com>
2024-08-07 17:50:07 +0100

    Few stuff

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: mben-yah <mben-yah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 04:36:19 by mben-yah          #+#    #+#             */
-/*   Updated: 2024/07/25 07:12:21 by mben-yah         ###   ########.fr       */
+/*   Updated: 2024/08/07 17:47:58 by mben-yah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,32 @@
 
 void	*thread_subroutine(void *arg)
 {
-	(void) arg;
+	int	index;
+	char	*str;
+
+	index = *(int *)arg;
+	if (index == 0)
+		str = "OPS";
+	else
+		str = "Foo";
 	printf("We got into the thread!\n");
 	while (1)
-		printf("OPS\n");
+		printf("%s\n", str);
 	return NULL;
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t	thread_id;
+	pthread_t	thread_id[2];
+	int i[2] = {0, 1};
+	int	j;
 
-	pthread_create(&thread_id, NULL, thread_subroutine, NULL);
-	pthread_join(thread_id, NULL);
+	while (j < 2)
+	{
+		pthread_create(thread_id + j, NULL, thread_subroutine, i + j);
+		j++;
+	}
+	// pthread_join(thread_id, NULL);
 	while (1)
 		printf("---\n");
 }
